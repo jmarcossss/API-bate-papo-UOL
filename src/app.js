a569c50d7eafea4042d5a3e70ef195a2c663212d
jmarcossss <joaomalcantaraaa@gmail.com>
2023-01-16 14:54:36 -0300

    setando variáveis globais e fazendo a conexão ao mongodb

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-// Libraries
+// Todas as bibliotecas utiizadas
+// import axios forom "axios"
+import express from 'express'
+import cors from 'cors'
+import joi from 'joi'
+import dotenv from 'dotenv'
+import { MongoClient, ObjectId } from 'mongodb'
+import dayjs from 'dayjs'
+
+// Variáveis globais
+const PORTA = 5000
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+const boolF = false
+const boolT = true
+
+// Armazenamento de dados
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+// const db = mongoClient.db();
+let db
+
+
+// Conectando ao mongo
+mongoClient.connect().then(() => {
+    db = mongoClient.db();
+});
